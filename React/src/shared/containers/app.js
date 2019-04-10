0f0209e776d016cccfb8a4140691c0abd6bc36d6
gayan sanjaya <v-gsanjaya@expedia.com>
2019-04-10 14:48:46 +0530

    react-7

--- React/src/shared/containers/app.js
+++ React/src/shared/containers/app.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Header from "../components/common/header";
+import Loader from "../components/common/loader";
 import PostList from "../components/post/post-list";
 
 export default class App extends React.Component{
@@ -23,6 +24,7 @@ export default class App extends React.Component{
 			    {this.state.view == "Post" &&
 					<section class="main layout--center">
 			            <div class="main__content layout--wrapped">
+			            	<Loader/>
 			            	<PostList/>
 			            </div>
 			        </section>
