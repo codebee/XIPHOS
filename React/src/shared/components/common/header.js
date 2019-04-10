6ad8ac92e27192c52d64bfc70b086b008e6b247d
gayan sanjaya <v-gsanjaya@expedia.com>
2019-04-10 17:40:07 +0530

    react-12 show join button by using showJoinButton State

--- React/src/shared/components/common/header.js
+++ React/src/shared/components/common/header.js
@@ -6,7 +6,8 @@ export default class Header extends React.Component{
 		super(props)
 
 		this.state={
-			user:this.props.user
+			user:this.props.user,
+			showJoinButton:true
 		}
 
 		console.log(this.props);
@@ -14,28 +15,40 @@ export default class Header extends React.Component{
 
 	render(){
 		return(
-			<section className="header layout--center">
-	            <div className="header__content layout--wrapped">
-	                <div className="brand">
-	                    <a href="/"><div className="brand__name"><span>#</span>DEVRANT</div></a>
-	                </div>
+			<React.Fragment>	
+				<section className="header layout--center">
+		            <div className="header__content layout--wrapped">
+		                <div className="brand">
+		                    <a href="/"><div className="brand__name"><span>#</span>DEVRANT</div></a>
+		                </div>
 
-	                {/*User Profile*/}	                
-	                <div className="profile layout--center">
-	                    <div className="profile__picture">
-	                        <svg height="36" width="36">
-	                            <circle cx="18" cy="18" r="18" fill="#5c5f6f"></circle>
-	                        </svg>
-	                    </div>
-	                    <div className="profile__name">{this.state.user}</div>
-	                </div>
+		                {!this.state.showJoinButton &&
+		                	<div className="profile layout--center">
+			                    <div className="profile__picture">
+			                        <svg height="36" width="36">
+			                            <circle cx="18" cy="18" r="18" fill="#5c5f6f"></circle>
+			                        </svg>
+			                    </div>
+			                    <div className="profile__name">{this.state.user}</div>
+			                </div>
+		                }
 
-	                <div className="join">
-	                	{/*<span>Join</span> */}	                   
-	                    <span>Sign Out</span>
-	                </div>
-	            </div>
-	        </section>
+		                <div className="join">
+		                	{this.state.showJoinButton &&
+		                		<span>Join</span>
+		                	}
+		                	
+
+		                	{!this.state.showJoinButton &&
+		                		<span>Sign Out</span>
+		                	}	                   
+		                    
+		                </div>
+
+		               			          		                
+		            </div>
+		        </section>
+	        </React.Fragment>	
 		)
 	}
 }
